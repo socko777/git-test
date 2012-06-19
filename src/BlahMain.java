a6325b68affb4eb7f0ed24bdcf1c024b5ca127d9
hong yung <hongyung@gmail.com>
2012-06-19 15:19:02 +0800

    aaa

--- src/BlahMain.java
+++ src/BlahMain.java
@@ -3,9 +3,10 @@ public class BlahMain {
 	public static void main(String[] args) {
 		System.out.println("blah");
 		String a = null;
-		//npe in the next line
+		// npe in the next line
 		a.substring(0);
 		a = a;
 		a = "a";
+		a = "b";
 	}
 }
